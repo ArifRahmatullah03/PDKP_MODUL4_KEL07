89ce927133d7567b758566b2ab7298c8af81e430
rezaalharis <hxoxo983@gmail.com>
2023-05-08 19:30:14 +0700

    Signed-off-by: rezaalharis <hxoxo983@gmail.com>

--- Tugas Modul 4 (1).cpp
+++ Tugas Modul 4 (1).cpp
@@ -65,7 +65,16 @@ int main() {
             pelajaran.jenis_buku = "Buku Pelajaran";
             pelajaran.harga_buku = 100000;
             beli_buku(pelajaran);
-        } else if (pilihan == 3)
+        } else if (pilihan == 3) {
+            Buku agama;
+            agama.jenis_buku = "Buku Agama";
+            agama.harga_buku = 50000;
+            beli_buku(agama);
+        }else if (pilihan == 4) {
+            Buku komik;
+            komik.jenis_buku = "Buku Komik";
+            komik.harga_buku = 90000;
+            beli_buku(komik);
 
         } else if (pilihan == 5) {
             cout << "Terima kasih telah menggunakan program ini!" << endl;
