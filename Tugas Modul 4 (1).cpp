7e2b6531ab870b4ce9ec06db0aa9a1f39076eed8
unknown <roberthouse38>
2023-05-08 17:58:15 +0700

    haris

--- Tugas Modul 4 (1).cpp
+++ Tugas Modul 4 (1).cpp
@@ -70,11 +70,8 @@ int main() {
             agama.jenis_buku = "Buku Agama";
             agama.harga_buku = 50000;
             beli_buku(agama);
-        } else if (pilihan == 4) {
-            Buku komik;
-            komik.jenis_buku = "Buku Komik";
-            komik.harga_buku = 90000;
-            beli_buku(komik);
+        }
+
         } else if (pilihan == 5) {
             cout << "Terima kasih telah menggunakan program ini!" << endl;
             break;
